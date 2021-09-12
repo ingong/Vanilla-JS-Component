8d7d50ea7eb0a5f8605af0979143b4fb8d4e9e6d
leeinsong <insong1216@gmail.com>
2021-09-12 18:51:57 +0900

    step1 : 기능구현, 추상화 및 모듈화

---
+++ src/core/Component.js
@@ -0,0 +1,24 @@
+export default class Component {
+  $target;
+  $state;
+  constructor($target) {
+    this.$target = $target;
+    this.setup();
+    this.render();
+  }
+  setup() {}
+  template() {
+    return '';
+  }
+  render() {
+    this.$target.innerHTML = this.template();
+    this.setEvent();
+  }
+  setEvent() {}
+  // 새로운 상태를 만들때는 기존의 상태를 변경시키는 것이 아니라,
+  // 현재의 상태 + 새롭게 들어온 상태를 더해주고, 이를 새로운 상태로 정의한다
+  setState(newState) {
+    this.$state = { ...this.$state, ...newState };
+    this.render();
+  }
+}
